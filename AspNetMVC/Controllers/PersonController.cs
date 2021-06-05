c8dbfc0f38906f2ef087ce1c315be047f51e2933
A.K.M Maruful Islam <59158377+marufulDev@users.noreply.github.com>
2021-06-05 22:50:50 +0600

    Initial Commit with data table full CRUD

---
+++ AspNetMVC/Controllers/PersonController.cs
@@ -0,0 +1,90 @@
+﻿using AspNetMVC.Models;
+using AspNetMVC.Models.View_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        private DatabaseContext db;
+        public PersonController()
+        {
+            db = new DatabaseContext();
+        }
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            List<Person> person = new List<Person>();
+            person = db.People.Take(25).OrderByDescending(x=>x.BusinessEntityID).ToList();
+            return View(person);
+        }
+
+        public ActionResult Edit(int? businessEntityID, Person model)
+        {
+            Person person;
+            /* 
+             * if businessEntityID is greater than 0 then fetch the data from database with that businessEntityID
+             * other wise new object
+            */
+            person = businessEntityID > 0 ? person = db.People.Where(x => x.BusinessEntityID == businessEntityID).SingleOrDefault() : new Person();
+
+            PersonSaveViewModel personSaveViewModel = new PersonSaveViewModel
+            {
+                PersonType=person.PersonType,
+                NameStyle=person.NameStyle,
+                Title=person.Title,
+                FirstName=person.FirstName,
+                MiddleName=person.MiddleName,
+                LastName=person.LastName,
+                EmailPromotion=person.EmailPromotion,
+                rowguid=person.rowguid,
+                ModifiedDate=person.ModifiedDate
+            };
+            return View(personSaveViewModel);
+        }
+
+        
+        public ActionResult Save(int? businessEntityID, PersonSaveViewModel model)
+        {
+            Person person;
+            person = businessEntityID > 0 ? db.People.Where(x => x.BusinessEntityID == businessEntityID).SingleOrDefault() : new Person();
+
+            person.ModifiedDate = DateTime.Now;
+            person.PersonType = model.PersonType;
+            person.Title = model.Title;
+            person.FirstName = model.FirstName;
+            person.MiddleName = model.MiddleName;
+            person.LastName = model.LastName;
+            if (businessEntityID <= 0)
+            {
+                person.BusinessEntityID = db.People.Max(x => x.BusinessEntityID) + 1;
+                person.rowguid = Guid.NewGuid();
+                person.NameStyle = model.NameStyle;
+                person.EmailPromotion = 1;
+
+                db.People.Add(person);
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("index", "Person");
+        }
+        public ActionResult Delete (int? businessEntityID)
+        {
+            Person person;
+            person = db.People.Where(x => x.BusinessEntityID == businessEntityID).SingleOrDefault() ;
+            if(person == null)
+            {
+                throw new Exception("Data doesnot found");
+            }
+            db.People.Remove(person);
+            db.SaveChanges();
+            
+            return RedirectToAction("index", "Person");
+        }
+    }
+}
---
+++ AspNetMVC/Models/View Model/PersonSaveViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC.Models.View_Model
+{
+    public partial class PersonSaveViewModel
+    {
+
+        public int BusinessEntityID { get; set; }
+
+        [Required(ErrorMessage ="The Field is required.")]
+        [StringLength(2,ErrorMessage ="Person type can not be more than 2 character.")]
+        public string PersonType { get; set; }
+
+        public bool NameStyle { get; set; }
+
+        [Required(ErrorMessage = "The Field is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The Field is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The Field is required.")]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "The Field is required.")]
+        public string LastName { get; set; }
+
+        public int EmailPromotion { get; set; }
+        public System.Guid rowguid { get; set; }
+        public System.DateTime ModifiedDate { get; set; }
+    }
+}
